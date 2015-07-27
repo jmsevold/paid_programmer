37ea741be8e75f994ceae908478dc3701dbe24f8
Jonathan Sevold <jmsevold@gmail.com>
2015-07-26 19:03:09 -0700

    rails_helper fixed, started meta_program

--- programming_tests/0001/meta_program.rb
+++ programming_tests/0001/meta_program.rb
@@ -6,12 +6,22 @@
 
   where h = {"this" => "that", "that" => "other", "where" => "here"}
 
-  and m.this == "this", m.that == "other", m.where == "here"
+  and m.this == "this", 
+
+  m.that == "other", 
+
+  m.where == "here"
 
 =end
-
+=begin 
+# Write a class that takes a hash. When the names of the keys are called on an instance of this class as methods,
+the return value should be the value of that key in the hash.
+=end
 class MetaProgram
   def initialize(h)
     @h = h
   end
+
+ def method_missing
+ end
 end
