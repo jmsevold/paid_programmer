a38c2073e1bd8ddf9f7d3dd79526a3490904e23c
Jonathan Sevold <jmsevold@gmail.com>
2015-07-26 21:18:02 -0700

    last exercise complete

--- programming_tests/0001/transform2.rb
+++ programming_tests/0001/transform2.rb
@@ -8,12 +8,20 @@ Transform.new.count
 will be output. (See spec/test_0001_spec.rb to see the exact output)
 
 =end 
+
+
+
 class Transform2
 
   def initialize
-    @file =
+    @file = File.open('../input_files/transform2.txt')
   end
+  
   def count
-    # code goes here
+    name_hash = Hash.new(0)
+    @file.each { |name| name_hash[name.chomp] += 1}
+    p name_hash 
   end
 end
+
+
